dcf38adc8f26433251186cc718126f8eac688bc2
jxrxsx <jrslord@hotmail.com>
2020-05-15 15:51:09 -0300

    frontend do painel de admin

--- frontend/src/js/painel-admin.js
+++ frontend/src/js/painel-admin.js
@@ -1,3 +1,52 @@
-$(document).ready( function () {
-    $('#table-lojas').DataTable();
-} );
+const BASE_API_URL = 'http://localhost:3000/api';
+const PANEL_CONTROLLER = 'panel';
+const STORE_CONTROLLER = 'store';
+
+$(document).ready(function () {
+
+    $.ajax({
+        type: 'GET',
+        url: `${BASE_API_URL}/${PANEL_CONTROLLER}`,
+        dataType: 'json',
+        success: function (stores) {
+            console.log('dados retornados da api');
+            console.log(stores);
+            $('#table-lojas').DataTable({
+                data: stores,
+                columns: [
+                    { title: "ID" },
+                    { title: "Loja" },
+                    { title: "Responsável" },
+                    { title: "Celular" },
+                    { title: "Categoria" },
+                    { title: "Status" },
+                    { title: "Editar" },
+                    { title: "Deletar" },
+                ]
+        
+            });
+        },
+
+    });
+
+
+
+
+
+
+});
+
+    
+$('#deleteBtn').on('click', function () {
+    let id = $(this).attr('name');
+    alert(`id do botao: ${id}`);
+
+    $.ajax({
+        type: 'DELETE',
+        url: `${BASE_API_URL}/${STORE_CONTROLLER}/${id}`,
+        dataType: 'json',
+        success: function (response) {
+            console.log(response);
+        }
+    });
+});
